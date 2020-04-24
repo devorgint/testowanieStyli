67d6cb6a4dcf208714ac0e642a229a7d8cad6beb
Anna Lisik <macfanpl@interia.pl>
2020-04-24 17:19:49 +0200

    play with classes && functions

---
+++ scss/mso/disdi.js
@@ -0,0 +1,9 @@
+// Class
+
+$subscription_name = document.namespaceURI();
+var $network_ability_to_refrain_itself;
+var $a;
+var $b = PushSubscription($name);
+var sfn = function name() {}
+var sfn = function name2() {}
+var sfn2 = function sffn() {}
