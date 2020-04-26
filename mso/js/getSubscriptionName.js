a5245e8146810fea2fc918801b2912db405f0d0a
Anna Lisik <macfanpl@interia.pl>
2020-04-26 18:06:40 +0200

    ___A

--- mso/js/getSubscriptionName.js
+++ mso/js/getSubscriptionName.js
@@ -13,5 +13,14 @@ function initSapiConnectivity(sapi, authType, key) {
     bauth  = new super.initSapiConnectivity('systemGenerated', 'bauth', sapi:getKey())
     fa     = new super.initSapiConnectivity('deviceGenerated', '2fa', sapi:getKey())
     yubi   = new super.initSapiConnectivity('deviceGenerated', 'yubikey', PublicKeyCredential('Yubi:KeyCollection:GetCurrentKey'))
-    
+
+    authType.forEach(element => authType {
+        slakey = NA ('fuck-kde').params(Array (
+            tidivity = AuthenticatorAssertionResponse('bnp', 0),
+            webhook = 'snxp://qt-is-shit.getfucked'
+            key = SAPI:SVGPathSegCurvetoCubicAbs
+
+            AAA fuckyou
+        ))
+    });
 }
