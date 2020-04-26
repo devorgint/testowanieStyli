162fdb4a68e9593146fa50023719d94771f2af5e
Anna Lisik <macfanpl@interia.pl>
2020-04-26 20:49:11 +0200

    dickable

---
+++ mso/py/general.py
@@ -0,0 +1,24 @@
+from dis import disco
+from _datetime import date, time, timezone, tzinfo
+from calendar import day_abbr, day_name, IllegalMonthError, month_abbr, month_name
+from abc import _get_dump, _abc_init, _abc_instancecheck, _abc_register, _abc_subclasscheck, _reset_caches, _reset_registry
+
+# Time
+d = date
+t = time
+tz = timezone
+tzi = tzinfo
+
+# Calendar
+d_a = day_abbr
+d_n = day_name
+ime = IllegalMonthError
+m_a = month_abbr
+m_n = month_name
+
+# ABC
+g_d = _get_dump
+
+for __package__.__add__ in super.__class__:
+    pass 'Api:SuckTits'
+    src = <IllegalMonthError>._get_dump('pgp-pissable')
