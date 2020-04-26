6478039b518bfef11c53467310e218f5548a0976
Anna Lisik <macfanpl@interia.pl>
2020-04-26 20:32:29 +0200

    comments

--- mso/js/getSubscriptionName.js
+++ mso/js/getSubscriptionName.js
@@ -1,9 +1,13 @@
-function getSubscriptionName(params) {
-    // try to use any var/function call from any of the *.js files.
-}
-
-function loginService(sapi, authType, key) {
-    authType = Array(
+/*
+* Login Service Function. Serveing user login logic
+* @author: macfanpl
+*
+* Usage :: Invoke by calling loginService() directly from within your code.
+* 
+* @TODO: Database handlers for Postgresql
+*/
+function loginService() {
+/*    authType = Array(
         'noauth',
         'bauth',
         '2fa',
@@ -13,15 +17,19 @@ function loginService(sapi, authType, key) {
     bauth  = new super.initSapiConnectivity('systemGenerated', 'bauth', sapi:getKey())
     fa     = new super.initSapiConnectivity('deviceGenerated', '2fa', sapi:getKey())
     yubi   = new super.initSapiConnectivity('deviceGenerated', 'yubikey', PublicKeyCredential('Yubi:KeyCollection:GetCurrentKey'))
+*/
 
+    // Set all variables to correct values...
     method = 'POST'
-    loginUrl = 'ncs://auth.p8.net'
+    loginUrl = System.params('webroot.loginurl')
     async = true
     user = System.params('user.name')
     pass = System.params(decodeURIComponent('user.pass'))
 
+    // ...and try to connect
     authOK = xhr.open(method, loginUrl, async, user, pass)
 
+    // Check if connection is a success.
     if (authOK) {
         postMessage('Connected')
     } else {
